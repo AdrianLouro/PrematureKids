52f86590b0070d87c64fc367d3f71032e992cffc
Adrián <alouroalonso_95@hotmail.com>
2018-12-19 16:53:18 +0000

    Dates bug fixed.

--- src/app/services/date.service.ts
+++ src/app/services/date.service.ts
@@ -8,30 +8,10 @@ export class DateService {
   constructor() { }
 
   apiDateTimeToIonDateTimeStringDate(apiDateTime: any): string {
-    return new Date(apiDateTime).toISOString();
+    return apiDateTime + '.000Z';
   }
 
   ionDateTimeDateToUTC(ionDateTimeDate: any): Date {
-    console.log(0, ionDateTimeDate);
-
-    if (!this.dateIsValid(ionDateTimeDate)) {
-      console.log(1, ionDateTimeDate.day.value);
-      console.log(2, ionDateTimeDate.month.value);
-      console.log(3, ionDateTimeDate.year.value);
-      console.log(4, new Date(Date.UTC(
-        ionDateTimeDate.year.value,
-        ionDateTimeDate.month.value - 1,
-        ionDateTimeDate.day.value,
-        0,
-        0,
-        0,
-        0
-      )));
-    } else {
-      console.log(5, ionDateTimeDate);
-      console.log(6, new Date(ionDateTimeDate));
-    }
-
     return !this.dateIsValid(ionDateTimeDate) ? new Date(Date.UTC(
       ionDateTimeDate.year.value,
       ionDateTimeDate.month.value - 1,
